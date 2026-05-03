96547340e49a3c9d5ea0be281833be68382d1a57
FelipeTbraun <tbraunfelipe@gmail.com>
2026-05-03 01:14:04 -0300

    Arrumando forms(n pronto)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Forms from "./pages/Forms";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/forms" element={<Forms />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Forms.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+
+function Forms(){
+    return(
+        <>
+  <Navbar />
+  </>
+    );
+}
+
+export default Forms;
